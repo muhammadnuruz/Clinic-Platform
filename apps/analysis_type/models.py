b44cdd67e7b33ea5a1bbd710bf926999aa29dca4
muhammadnur <muhammadnurpdp@gmail.com>
2024-09-29 20:43:31 +0500

    First Commit

--- apps/analysis_type/models.py
+++ apps/analysis_type/models.py
@@ -2,10 +2,28 @@ from django.db import models
 
 
 class Types(models.Model):
+    CATEGORY_CHOICES = [
+        ('gormon', 'Gormonal tekshiruvlar'),
+        ('bioximik', 'Bioximik tekshiruvlar'),
+        ('bakteriologik', 'Bakteriologik tekshiruvlar'),
+        ('psr', 'PSR tekshiruvlar'),
+        ('klinik', 'Klinik laborator tekshiruvlar'),
+        ('mikroskopik', 'Mikroskopik tekshiruvlar'),
+    ]
+
+    RU_CATEGORY_CHOICES = [
+        ('gormon', 'Гормональные исследования'),
+        ('bioximik', 'Биохимические исследования'),
+        ('bakteriologik', 'Бактериологические исследования'),
+        ('psr', 'ПЦР исследования'),
+        ('klinik', 'Клинические лабораторные исследования'),
+        ('mikroskopik', 'Микроскопические исследования'),
+    ]
+
     name = models.CharField(max_length=100)
     ru_name = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
-    ru_category = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
+    ru_category = models.CharField(max_length=100, choices=RU_CATEGORY_CHOICES)
     price = models.IntegerField()
     info = models.TextField()
     ru_info = models.TextField()
