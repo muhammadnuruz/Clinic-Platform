4fe2ddc0f226c23fa4eeb712970c2de65c52b6c1
muhammadnur <muhammadnurpdp@gmail.com>
2024-11-12 20:08:47 +0500

    First Commit

--- ArzonLabaratoriya/settings.py
+++ ArzonLabaratoriya/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c8@18*sl-_eb0!#3vz(87!)&#ldmska)-=&opra#59z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
